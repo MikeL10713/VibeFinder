b761fc2329acd341ced43b96bb9ce4c2ad804530
MikeL10713 <112588056+MikeL10713@users.noreply.github.com>
2022-12-17 12:46:11 -0600

    Created backend with flask to connect to frontend

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import UserInput from "./components/UserInput";
+import SearchedArtist from "./components/SearchedArtist";
+import SearchedSong from "./components/SearchedSong";
+import RecommendedTracks from "./components/RecommendedTracks";
+
+const App = () => {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/test").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+      {(typeof data.members === "undefined") ? (
+        <p>Loading...</p>
+      ) : (
+        data.members.map((member, i) => (<p key={i}>{member}</p>))
+      )}
+
+      <h1 style={{marginLeft: 10}}>VibeFinder</h1>
+      <UserInput />
+      <br/>
+      <div style={{display: "flex"}}>
+        <SearchedArtist />
+        <SearchedSong />
+      </div>
+      <br/>
+      <RecommendedTracks />
+    </div>
+  )
+}
+
+export default App
