be290daf80c1ed3b993abdc5ccba4d80ddf79a52
MikeL10713 <112588056+MikeL10713@users.noreply.github.com>
2022-12-18 19:25:51 -0600

    added functionality of recommending tracks, currently list of ids for the tracks

--- client/src/components/UserInput.js
+++ client/src/components/UserInput.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 
 const Artistinput = () => {
   const [artist, setArtist] = useState("")
+  const [trackCount, setTrackCount] = useState("")
   const [track, setTrack] = useState("")
   const [trackArtist, setTrackArtist] = useState("")
 
@@ -12,6 +13,10 @@ const Artistinput = () => {
       alert("Please choose an artist")
       return
     }
+    if (!trackCount) {
+      alert("Please specify how many tracks you want")
+      return
+    }
     if (!track) {
       alert("Please choose a track")
       return
@@ -20,8 +25,9 @@ const Artistinput = () => {
       alert("Please specify the track's artist")
       return
     }
-
+    
     setArtist("")
+    setTrackCount("")
     setTrack("")
     setTrackArtist("")
   }
@@ -29,28 +35,35 @@ const Artistinput = () => {
   return (
     <div>
       <form onSubmit={onSubmit}> 
-        <input style={{marginLeft: 10, width: 300}}
+        <input style={{marginLeft: 10, width: 250}}
           type="text"
           placeholder="Artist that you want tracks from"
           value={artist}
           name="artist"
           onChange={(e) => setArtist(e.target.value)} 
         />
-        <input style={{marginLeft: 10, width: 300}}
+        <input style={{marginLeft: 10, width: 250}}
+          type="text"
+          placeholder="How many tracks do you want? 1-5"
+          value={trackCount}
+          name="trackCount"
+          onChange={(e) => setTrackCount(e.target.value)} 
+        />
+        <input style={{marginLeft: 10, width: 250}}
           type="text"
           placeholder="Track with the vibes you're looking for"
           value={track}
           name="track"
           onChange={(e) => setTrack(e.target.value)} 
         />
-        <input style={{marginLeft: 10, width: 300}}
+        <input style={{marginLeft: 10, width: 250}}
           type="text"
           placeholder="Artist that made your track"
           value={trackArtist}
           name="trackArtist"
           onChange={(e) => setTrackArtist(e.target.value)} 
         />
-        <input style={{marginLeft: 10, width: 200}}
+        <input style={{marginLeft: 10, width: 175}}
           type="submit"
           value="Find some vibes!"
         />
--- flask-server/recommendations.py
+++ flask-server/recommendations.py
@@ -1,5 +1,6 @@
 import requests
-from clientCredentials import client_id, client_secret
+from clientCredentials import client_id, client_secret 
+import math
 
 # setup for making API calls
 data = {
@@ -16,7 +17,8 @@ headers = {
 # these will be taken from user input
 target_track_name = "message in a bottle"
 target_track_artist = "taylor swift"
-new_artist = "michae jackson"
+new_artist = "lauv"
+recommendation_count = 3
 
 # searching for target track given name and artist by user
 querystring = {"q": target_track_name + " artist:" + target_track_artist,"type":"track"}
@@ -37,8 +39,8 @@ unique_album_names = set()
 album_ids = []
 for al in albums:
   # because our limit is set to 50 in order to make sure we capture all of an artist's albums, 
-      # extra albums are added to make the result reach 50
-  # thus, we have to make sure our chosen artist actually contributed to the album before we add it 
+      # extra albums are added to make the result reach 50 albums
+  # thus, we have to make sure our chosen artist actually contributed to an album before we add it 
   artists_of_this_album = []
   for ar in al.get("artists"):
     artists_of_this_album.append(ar.get("name"))
@@ -59,9 +61,64 @@ for id in album_ids:
       all_tracks.append(tr)
     unique_track_names.add(tr.get("name"))
 
-for t in all_tracks: print(t.get("name"))
-print(len(all_tracks))
+# getting the audio features of the target track
+response = requests.get("https://api.spotify.com/v1/audio-features/" + target_track.get("id"),headers=headers)
+target_audio_features = response.json()
+target_danceability = target_audio_features.get("danceability")
+target_energy = target_audio_features.get("energy")
+target_valence = target_audio_features.get("valence")
 
-  
+# selecting the three tracks with the least euclidean distance from the target track to be recommended
+# making a dict to map track ids to their euclidean distance with placeholder values
+recommended_track_ids = dict()
+for i in range(0, recommendation_count): recommended_track_ids[i] = 13
+# getting all track ids to iterate through
+all_track_ids = []
+for tr in all_tracks:
+  all_track_ids.append(tr.get("id"))
+while all_track_ids:
+  # we have to process the tracks in batches of 100 (api limit for getting audio features)
+  if len(all_track_ids) > 100:
+    ids = ",".join(all_track_ids[:100])
+    querystring = {"ids":ids}
+    response = requests.get("https://api.spotify.com/v1/audio-features",headers=headers,params=querystring)
+    response_audio_features = response.json().get("audio_features")
+    for track in response_audio_features:
+      danceability = track.get("danceability")
+      energy = track.get("energy")
+      valence = track.get("valence")
+      euclidean_distance = math.sqrt(pow((target_danceability - danceability), 2) + pow((target_energy - energy), 2) + pow((target_valence - valence), 2))
+      # go through and replace the largest euclidean distance in the dict if the current one is smaller
+      largest_euclidean_distance = 0
+      corresponding_slot = 0
+      for slot in recommended_track_ids:
+        if recommended_track_ids[slot] > largest_euclidean_distance:
+          largest_euclidean_distance = recommended_track_ids[slot]
+          corresponding_slot = slot
+      if euclidean_distance < largest_euclidean_distance:
+        recommended_track_ids.pop(corresponding_slot)
+        recommended_track_ids[track.get("id")] = euclidean_distance
+    all_track_ids = all_track_ids[100:]
+  else:        
+    ids = ",".join(all_track_ids)
+    querystring = {"ids":ids}
+    response = requests.get("https://api.spotify.com/v1/audio-features",headers=headers,params=querystring)
+    response_audio_features = response.json().get("audio_features")
+    for track in response_audio_features:
+      danceability = track.get("danceability")
+      energy = track.get("energy")
+      valence = track.get("valence")
+      euclidean_distance = math.sqrt(pow((target_danceability - danceability), 2) + pow((target_energy - energy), 2) + pow((target_valence - valence), 2))
+      # go through and replace the largest euclidean distance in the dict if the current one is smaller
+      largest_euclidean_distance = 0
+      corresponding_slot = 0
+      for slot in recommended_track_ids:
+        if recommended_track_ids[slot] > largest_euclidean_distance:
+          largest_euclidean_distance = recommended_track_ids[slot]
+          corresponding_slot = slot
+      if euclidean_distance < largest_euclidean_distance:
+        recommended_track_ids.pop(corresponding_slot)
+        recommended_track_ids[track.get("id")] = euclidean_distance
+    all_track_ids = []
 
-
+print(recommended_track_ids)
