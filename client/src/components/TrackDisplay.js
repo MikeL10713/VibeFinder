425f58f7081b3f1c981ec72d0a71f481bd76d64f
MikeL10713 <112588056+MikeL10713@users.noreply.github.com>
2022-12-22 18:50:11 -0600

    added audio preview for displayed tracks

--- client/src/components/TrackDisplay.js
+++ client/src/components/TrackDisplay.js
@@ -1,8 +1,18 @@
-const TrackDisplay = ({ trackName, trackAlbumCoverUrl, trackAlbum, trackArtists }) => {
+const TrackDisplay = ({ trackName, trackAlbumCoverUrl, trackAlbum, trackArtists, trackAudioUrl, audioPlaybackId }) => {
+  const playAudio = () => {
+    const audio = document.getElementsByClassName(audioPlaybackId)[0]
+    audio.play()
+  }
+
   return (
     <div>
       <h1>{trackName}</h1>
       <img src={trackAlbumCoverUrl} alt="" />
+      <br/>
+      <button onClick={playAudio}>30-second preview of the vibes</button>
+      <audio className={audioPlaybackId}>
+        <source src={trackAudioUrl} />
+      </audio>
       <h2>{trackAlbum}</h2>
       <h2>{trackArtists}</h2>
     </div>
